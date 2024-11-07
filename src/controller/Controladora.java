d136d7c209cf1d1faea97b75402e42555458f6f1
CristianJDSC <cdesousa4562@gmail.com>
2024-11-07 18:56:59 -0400

    prueba

--- src/controller/Controladora.java
+++ src/controller/Controladora.java
@@ -18,6 +18,7 @@ import static javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE;
 public class Controladora {
     JFrame ventana;
     int holaaaaa;
+    int aaaaaaaa;
     
     public Controladora(JFrame ventana){
        this.ventana = ventana;
